439012efe8bc90988b50851eaece2d8fdd5ac577
Kayra Coskun <kayra.coskun@outlook.com.tr>
2022-08-12 23:48:38 +0300

    changed markers of plots

--- Plots/line_plot.py
+++ Plots/line_plot.py
@@ -6,8 +6,9 @@ import data
 # Time Plot
 plt.figure(figsize=(9.6, 7.2), dpi=120)
 plt.subplot(2, 1, 1)
-plt.plot(data.pop_x, data.y_poptime1, data.pop_x,
-         data.y_poptime2, data.pop_x, data.y_poptime3, marker="o")
+plt.plot(data.pop_x, data.y_poptime1, marker="*")
+plt.plot(data.pop_x, data.y_poptime2, marker="^")
+plt.plot(data.pop_x, data.y_poptime3, marker="o")
 
 plt.title("Population Size Graph")
 #plt.xlabel("Population Size")
@@ -20,8 +21,9 @@ plt.yticks(np.arange(0, 0.12, 0.01))
 
 # Length Plot
 plt.subplot(2, 1, 2)
-plt.plot(data.pop_x, data.y_poplen1, data.pop_x,
-         data.y_poplen2, data.pop_x, data.y_poplen3, marker="o")
+plt.plot(data.pop_x, data.y_poplen1, marker="*")
+plt.plot(data.pop_x, data.y_poplen2, marker="^")
+plt.plot(data.pop_x, data.y_poplen3, marker="o")
 
 plt.xlabel("Population Size")
 plt.ylabel("Path Length (meter)")
@@ -37,8 +39,9 @@ plt.show()
 # Time plots
 plt.figure(figsize=(9.6, 7.2), dpi=120)
 plt.subplot(2, 1, 1)
-plt.plot(data.mut_x, data.y_muttime1, data.mut_x,
-         data.y_muttime2, data.mut_x, data.y_muttime3, marker="o")
+plt.plot(data.mut_x, data.y_muttime1, marker="*")
+plt.plot(data.mut_x, data.y_muttime2, marker="^")
+plt.plot(data.mut_x, data.y_muttime3, marker="o")
 
 plt.title("Mutation Rate Graph")
 #plt.xlabel("Population Size")
@@ -49,8 +52,9 @@ plt.legend(["Test 1", "Test 2", "Test 3"], loc="upper right")
 
 # Length Plot
 plt.subplot(2, 1, 2)
-plt.plot(data.mut_x, data.y_mutlen1, data.mut_x,
-         data.y_mutlen2, data.mut_x, data.y_mutlen3, marker="o")
+plt.plot(data.mut_x, data.y_mutlen1, marker="*")
+plt.plot(data.mut_x, data.y_mutlen2, marker="^")
+plt.plot(data.mut_x, data.y_mutlen3, marker="o")
 
 plt.xlabel("Mutation Rate")
 plt.ylabel("Path Length (meter)")
@@ -61,40 +65,41 @@ plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
 plt.show()
 
 
-# Chromosome length line plots
-# Time plots
-plt.figure(figsize=(9.6, 7.2), dpi=120)
-plt.subplot(2, 1, 1)
-plt.plot(data.chr_x, data.y_chrtime1, data.chr_x,
-         data.y_chrtime2, data.chr_x, data.y_chrtime3, marker="o")
+# # Chromosome length line plots
+# # Time plots
+# plt.figure(figsize=(9.6, 7.2), dpi=120)
+# plt.subplot(2, 1, 1)
+# plt.plot(data.chr_x, data.y_chrtime1, data.chr_x,
+#          data.y_chrtime2, data.chr_x, data.y_chrtime3, marker="o")
 
-plt.title("Chromosome Length Graph")
-#plt.xlabel("Population Size")
-plt.ylabel("Computation Time (second)")
+# plt.title("Chromosome Length Graph")
+# #plt.xlabel("Population Size")
+# plt.ylabel("Computation Time (second)")
 
-plt.grid(linestyle="--")
-plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
+# plt.grid(linestyle="--")
+# plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
 
-# Length Plot
-plt.subplot(2, 1, 2)
-plt.plot(data.chr_x, data.y_chrlen1, data.chr_x,
-         data.y_chrlen2, data.chr_x, data.y_chrlen3, marker="o")
+# # Length Plot
+# plt.subplot(2, 1, 2)
+# plt.plot(data.chr_x, data.y_chrlen1, data.chr_x,
+#          data.y_chrlen2, data.chr_x, data.y_chrlen3, marker="o")
 
-plt.xlabel("Chromosome Length")
-plt.ylabel("Path Length (meter)")
+# plt.xlabel("Chromosome Length")
+# plt.ylabel("Path Length (meter)")
 
-plt.grid(linestyle="--")
-plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
+# plt.grid(linestyle="--")
+# plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
 
-plt.show()
+# plt.show()
 
 
 # Stop criterion line plots
 # Time plots
 plt.figure(figsize=(9.6, 7.2), dpi=120)
 plt.subplot(2, 1, 1)
-plt.plot(data.st_x, data.y_sttime1, data.st_x,
-         data.y_sttime2, data.st_x, data.y_sttime3, marker="o")
+plt.plot(data.st_x, data.y_sttime1, marker="*")
+plt.plot(data.st_x, data.y_sttime2, marker="^")
+plt.plot(data.st_x, data.y_sttime3, marker="o")
 
 plt.title("Stop Criterion Graph")
 #plt.xlabel("Population Size")
@@ -105,8 +110,9 @@ plt.legend(["Test 1", "Test 2", "Test 3"], loc="lower right")
 
 # Length Plot
 plt.subplot(2, 1, 2)
-plt.plot(data.st_x, data.y_stlen1, data.st_x,
-         data.y_stlen2, data.st_x, data.y_stlen3, marker="o")
+plt.plot(data.st_x, data.y_stlen1, marker="*")
+plt.plot(data.st_x, data.y_stlen2, marker="^")
+plt.plot(data.st_x, data.y_stlen3, marker="o")
 
 plt.xlabel("Stop Criterion")
 plt.ylabel("Path Length (meter)")
